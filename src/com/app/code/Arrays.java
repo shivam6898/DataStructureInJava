6df4b0f04cd9067752177326c08250e6ae49eaca
shivam727 <shivampatil727@gmail.com>
2020-09-28 15:22:09 +0530

    Arrays file commited

---
+++ src/com/app/code/Arrays.java
@@ -0,0 +1,20 @@
+package com.app.code;
+
+public class Arrays {
+
+	
+	public static void main(String[] args) {
+     int[] intarray= new int[7];
+     intarray[0]=10;
+     intarray[1]=20;
+     intarray[2]=30;
+     intarray[3]=20;
+     intarray[4]=20;
+     intarray[5]=20;
+     
+     
+    for (int i = 0; i < intarray.length; i++) {
+		System.out.println(intarray[i]);
+	}
+	}
+}
